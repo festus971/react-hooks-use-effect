0b13e1c8c9c9f952caa167281951bb1f90ace198
+ <+>
2022-07-15 15:47:06 +0300

    commit tree e4eb8e0bde88dd473bd528d34017063f1900daac

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React, { useState, useEffect } from "react";
+import DogPics from "./DogPics";
 
 function App() {
+  const [count, setCount]=useState(0);
+  const [text, setText]=useState("");
   useEffect(() => {
     console.log("useEffect called");
-  });
+  },[text]);
+  useEffect(()=>{
+    setTimeout(()=>setCount(0),5000);
+  },[]);
 
   console.log("Component rendering");
 
-  return <button>Click Me</button>;
+  return (
+  <div>
+    < DogPics/>
+    <button onClick={()=>setCount((count)=>count +1)}>I have been clicked {count}</button>;
+    <input type="text" placeholder="Type away..." value={text} onChange={(e)=>setText(e.target.value)}/>
+    </div>
+  );
 }
 
 export default App;
